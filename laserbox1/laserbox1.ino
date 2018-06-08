8829c45283d2422f386ed90f4c9ab5219f0c0969
pizzamakerca <39610227+pizzamakerca@users.noreply.github.com>
2018-06-08 07:59:36 -0700

    Fixing formatting errors and a few more comments.

--- laserbox1/laserbox1.ino
+++ laserbox1/laserbox1.ino
@@ -1,27 +1,28 @@
 /*
-* Ethernet_Test.ino - Simple sketch to listen for E1.31 data on an 
-*                     UNO Ethernet shield and print some statistics.
-*
-* Project: E131 - E.131 (sACN) library for Arduino
-* Copyright (c) 2015 Shelby Merrick
-* http://www.forkineye.com
-*
-*  This program is provided free for you to use in any way that you wish,
-*  subject to the laws and regulations where you are using it.  Due diligence
-*  is strongly suggested before using this code.  Please give credit where due.
-*
-*  The Author makes no warranty of any kind, express or implied, with regard
-*  to this program or the documentation contained in this document.  The
-*  Author shall not be liable in any event for incidental or consequential
-*  damages in connection with, or arising out of, the furnishing, performance
-*  or use of these programs.
-*
-*/
+ * Ethernet_Test.ino - Simple sketch to listen for E1.31 data on an 
+ *                     UNO Ethernet shield and print some statistics.
+ *
+ * Project: E131 - E.131 (sACN) library for Arduino
+ * Copyright (c) 2015 Shelby Merrick
+ * http://www.forkineye.com
+ *
+ *  This program is provided free for you to use in any way that you wish,
+ *  subject to the laws and regulations where you are using it.  Due diligence
+ *  is strongly suggested before using this code.  Please give credit where due.
+ *
+ *  The Author makes no warranty of any kind, express or implied, with regard
+ *  to this program or the documentation contained in this document.  The
+ *  Author shall not be liable in any event for incidental or consequential
+ *  damages in connection with, or arising out of, the furnishing, performance
+ *  or use of these programs.
+ *
+ */
 
 /*
  * This is the file for Laser Box 0.
  */
 
+// Setup #INCLUDE statements for required libraries.
 #include <SPI.h>
 #include <Ethernet.h>
 #include <E131.h>
@@ -29,7 +30,7 @@
 // Change only this value to setup a new laserbox.
 #define LASERBOX 0
 
-// Define CONSTANTS
+// Define CONSTANTS used for dmx and relay conrol.
 #define UNIVERSE 1
 #define RELAY1 2
 #define RELAY2 3
@@ -40,47 +41,49 @@
 #define RELAY7 8
 #define RELAY8 9
 
+
 // Initialiaze an E131 object.
 E131 e131;
 
 
 void setup() {
-    // Configure digital output pins.
-    pinMode(RELAY1, OUTPUT);
-    pinMode(RELAY2, OUTPUT);
-    pinMode(RELAY3, OUTPUT);
-    pinMode(RELAY4, OUTPUT);
-    pinMode(RELAY5, OUTPUT);
-    pinMode(RELAY6, OUTPUT);
-    pinMode(RELAY7, OUTPUT);
-    pinMode(RELAY8, OUTPUT);
+  // Configure digital output pins.
+  pinMode(RELAY1, OUTPUT);
+  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAY3, OUTPUT);
+  pinMode(RELAY4, OUTPUT);
+  pinMode(RELAY5, OUTPUT);
+  pinMode(RELAY6, OUTPUT);
+  pinMode(RELAY7, OUTPUT);
+  pinMode(RELAY8, OUTPUT);
 
-    // Ensure digital outputs are off (HIGH in the case of relays).
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    digitalWrite(9, HIGH);
+  // Ensure digital outputs are off (HIGH in the case of relays).
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
 
-    // Setup the network interface.
-    byte macLastByte = LASERBOX;
-    byte mac[] = { 0xDE, 0xAD, 0xBE, 0x2F, 0x1E, macLastByte };
-    IPAddress dnsAddr(192, 168, 1, 10);
-    IPAddress gateway(192, 168, 1, 10);
-    IPAddress subnet(255, 255, 255, 0);
-    int ipLastOctet = 20 + LASERBOX;
-    IPAddress ip(192, 168, 1, ipLastOctet);
+  // Setup the network interface.
+  byte macLastByte = LASERBOX;
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0x2F, 0x1E, macLastByte };
+  IPAddress dnsAddr(192, 168, 1, 10);
+  IPAddress gateway(192, 168, 1, 10);
+  IPAddress subnet(255, 255, 255, 0);
+  int ipLastOctet = 20 + LASERBOX;
+  IPAddress ip(192, 168, 1, ipLastOctet);
 
 
-    /* Configure from settings above and listen 
-     * for Unicast broadcast packets on UDP
-     * port 5568.
-     */
+  /* Configure from settings above and listen 
+   * for Unicast broadcast packets on UDP
+   * port 5568.
+   */
     e131.begin(mac, ip, subnet, gateway, dnsAddr);
 }
+
 
 void setOutput(int dmx, int value) {
   int channel;
@@ -139,6 +142,7 @@ void setOutput(int dmx, int value) {
       break;
   }
 }
+
 
 void loop() {
   /* Parse a packet */
